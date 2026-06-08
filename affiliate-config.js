ac4344d9183cf9076f36ceae9bd862331167ec60
Alex803161 <visitaevaleksej@gmail.com>
2026-06-08 19:42:00 +0300

    Update affiliate-config.js

--- affiliate-config.js
+++ affiliate-config.js
@@ -8,19 +8,12 @@ const AFFILIATE = {
   },
   bukvoed: {
     base: 'https://www.bukvoed.ru/search?q=',
-    param: 'partner',     // или другой параметр
-    id: ''                // ваш ID
+    param: 'partner',
+    id: ''
   },
-  // Можно добавить MyBook, Строки и т.д.
-  defaultStore: 'litres'  // какой магазин по умолчанию
+  defaultStore: 'litres'
 };
 
-/**
- * Получить партнёрскую ссылку на покупку книги
- * @param {string} title - название книги для поиска
- * @param {string} store - 'litres' или 'bukvoed' (по умолчанию из defaultStore)
- * @returns {string} ссылка
- */
 function getBuyLink(title, store = AFFILIATE.defaultStore) {
   const shop = AFFILIATE[store];
   if (!shop) return '#';
